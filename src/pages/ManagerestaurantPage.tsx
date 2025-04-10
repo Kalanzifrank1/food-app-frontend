b8936ac68567cc2e00d71404ef14b9eed40c86d2
Kalanzifrank1 <kalanzifrank5@gmail.com>
2025-04-10 05:11:25 -0700

    adding the type on order

--- src/pages/ManagerestaurantPage.tsx
+++ src/pages/ManagerestaurantPage.tsx
@@ -5,6 +5,7 @@ import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestauran
 import { useCreateMyRestaurant, useGetMyRestaurant, useGetMyRestaurantOrders, useUpdateRestaurant } from "@/api/MyRestaurantapi";
 import { TabsList, Tabs, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import OrderItemCard from "../components/OrderItemCard";
+import { Order } from "@/types";
 
 const ManageRestaurantPage = () => {
     const navigate = useNavigate();
@@ -30,7 +31,7 @@ const ManageRestaurantPage = () => {
             </TabsList>
             <TabsContent value="orders" className="space-y-5 bg-gray-50 py-10 rounded-lg">
                 <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
-                {orders?.map((order) => (
+                {orders?.map((order: Order) => (
                     <OrderItemCard order={order} />
                 ))}
             </TabsContent>
